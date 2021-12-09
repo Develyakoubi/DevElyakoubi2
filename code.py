980a4cfe595b4265b287cb4b1d0d523eb87cc29e
DevElyakoubi <mohamedelyakoubi414@gmail.com>
2021-12-09 16:35:53 +0100

    test

---
+++ code.py
@@ -0,0 +1,15 @@
+def func(number):
+    print ("enter nymber ")
+
+    if (number%2==0)
+    {
+    	print("the nubre is "+number +"even")
+    }
+
+    else
+    {
+    	print("the number is "+number + "odd")
+    }
+
+
+
